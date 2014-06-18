7587b557204d3bf930dd6443592312a0c1de3d54
yamasaki-chachi <t.yamasaki11@gmail.com>
2014-06-19 05:59:13 +0900

    障害物ノードのコストをCOST_MAXにするメソッド追加

--- yamasaki/ControllerClass/Controller.cpp
+++ yamasaki/ControllerClass/Controller.cpp
@@ -14,8 +14,18 @@ int main(){
     std::vector<int> route;
     
     Block block;
-    block.calcRoute(0.0, 0.0, UP, 8);
-    route = block.getRouteCoordinate();
+    block.calcRoute(0.0, 0.0, UP, 1);
+    route = block.getMovePointList();
+    
+    std::cout << "点" << std::endl;
+	for(int i=0; i<route.size(); i++)
+    {
+        std::cout << route[i] << std::endl;
+    }
+    
+    block.setMeshImpassable(660.0, 0.0);
+    block.calcRoute(330.0, 0.0, UP, 3);
+    route = block.getMovePointList();
     
     std::cout << "点" << std::endl;
 	for(int i=0; i<route.size(); i++)
