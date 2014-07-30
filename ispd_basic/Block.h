7e409fc1ce4c89fc54320bb5a8a6564361376c59
yamasaki-chachi <t.yamasaki11@gmail.com>
2014-07-30 22:07:23 +0900

    ispd_basicにドッキングコードを結合

--- ispd_basic/Block.h
+++ ispd_basic/Block.h
@@ -36,7 +36,7 @@ enum SearchMark
     UNKNOWN,    //未探索
     PASSABLE,   //通れる
     IMPASSABLE  //通れない
-};
+};     
 
 /*****************************************************************************
  ** Structure
@@ -45,6 +45,7 @@ struct Mesh{
     int num;            //メッシュ番号
     Coordinate center;     //メッシュの中心座標(x,y)
     SearchMark mark;    //フィールドの探索情報を入れるマーカー
+    int ir_value;       //IR値
 };
 
 struct Wall{
@@ -68,6 +69,11 @@ public:
     /*Dijkstraクラスをインスタンス生成し，ダイクストラ法でルートを計算させる
     　探索ルートの座標値をリストにして返す(長いので処理はBlock.cpp内に書く)*/
     std::vector<Coordinate> getMovePointList(Coordinate, IRobotDirection direction, int);
+    
+    /*Dijkstraクラスをインスタンス生成し，ダイクストラ法でDockまでのルートを計算させる
+    　探索ルートの座標値をリストにして返す(長いので処理はBlock.cpp内に書く)*/
+    std::vector<Coordinate> getMovePointListToDock(Coordinate, IRobotDirection direction);
+
     //次に向かうノード番号を取得する
     int getNextMeshNum()
     {
@@ -118,6 +124,15 @@ public:
         num = meshNumToCurrentPosition(coord);
         block[num/total_block_x][num%total_block_x].mark = mark;
     }
+
+    //IR値をメッシュにセットする
+    void setIRMark(Coordinate coord, int value)
+    {
+	int num;
+
+        num = meshNumToCurrentPosition(coord);
+        block[num/total_block_x][num%total_block_x].ir_value = value;
+    }
     
 private:
     //変数
@@ -166,7 +181,17 @@ private:
 例)
  cost[0][1]:ノード番号0から1に向かう道のコスト
  cost[1][0]:ノード番号1から0に向かう道のコスト
- 
+
+ ・ドッキングステーションについて
+ value      Name
+240         Reserved
+248         Red Buoy
+244         Green Buoy
+242         Force Field
+252         Red Buoy and Green Buoy
+250         Red Buoy and Force Field
+246         Green Buoy and Force Field
+254         Red Buoy, Green Buoy and Force Field
  *****************************************************************************/
 
 
