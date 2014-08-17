b135b260a548ded64055aeab146f171e6c959197
nakazato-ispd <nakap509@gmail.com>
2014-08-17 12:06:47 +0900

    フォルダ整理

---
+++ yamasaki/OpenCV/Create.h
@@ -0,0 +1,255 @@
+#include <createoi.h>
+
+
+/*****************************************************************************
+ ** Define
+ *****************************************************************************/
+#define VELOCITY_H 200			//速度[mm/s]
+#define VELOCITY_L 100			//速度[mm/s]
+#define RADIUS_LEFT  230.0		//左回り回転半径[mm]
+#define RADIUS_RIGHT 320.0		//右回り回転半径[mm]
+#define WALL_DISTANCE_LOW  180.0	//壁とCreateの距離_低[cm]  straight curve用
+#define WALL_DISTANCE_HIGH 230.0	//壁とCreateの距離_高[cm]
+
+#define WALL_DIST_L 120	//壁とCreateの距離_低[cm]	straight turn 用
+#define WALL_DIST_H 190	//壁とCreateの距離_高[cm]
+#define AVE_DIST ((WALL_DIST_H+WALL_DIST_L)/2)
+
+
+#define CREATE_SIZE 330 // 縦 [mm]
+// 横330[mm]
+
+// soner
+#define SONEROFFSET_X 		150 // [mm]
+#define SONEROFFSET_ANGLE	(-45 * (M_PI / 180)) 			//rad
+#define SONEROFFSET_ANGLE_L	((180-SONEROFFSET_ANGLE_R) * (M_PI / 180))	//[rad]
+// bumper
+#define BUMPER_PLACE_OFFSET 170 //[mm]
+#define BUMPER_PLACE_ANGLE_R  (45 * (M_PI / 180))		//[rad]
+#define BUMPER_PLACE_ANGLE_L  ((180-BUMPER_PLACE_ANGLE_R) * (M_PI / 180))
+
+// 超音波センサで障害物の座標を記録する際の閾値　この距離より遠い座標は記録しない
+#define RECORD_OBSTACLE_TH 900
+
+// 壁探索時の飛び値対策
+#define WALL_SEARCH_TH 500
+
+// 障害物の端に入ったときの検知閾値
+#define OBSTACLE_SEARCH_H_TH 300
+#define OBSTACLE_SEARCH_L_TH 170
+
+// 壁探索の直進回転走行の復帰回転角度
+#define RETURN_ANGLE 4
+
+// 回転誤差補正値
+#define P_TURN_A100 (-0.0003)
+//#define P_TURN_B100 0.1227
+#define P_TURN_B100 0.2027
+//#define P_TURN_C100 2.6985
+#define P_TURN_C100 3.3513
+
+#define N_TURN_A100 (0.0002)
+#define N_TURN_B100 0.12
+#define N_TURN_C100 (-3.3513)
+
+// 直進誤差補正値
+#define P_DRIVEX_A200 (-0.009189)
+#define P_DRIVEX_B200 9.82555
+#define P_DRIVEY_A200 (-0.030117)
+#define P_DRIVEY_B200 5.44736
+
+#define N_DRIVEX_A100 1
+#define N_DRIVEX_B100 -10.9
+#define N_DRIVEY_A100 -4.8
+
+
+
+/*****************************************************************************
+ ** enum
+ *****************************************************************************/
+
+enum Bumper
+{
+	NONE,
+	RIGHT,
+	LEFT,
+	CENTER
+};
+
+
+
+/*****************************************************************************
+ ** Classes
+ *****************************************************************************/
+class Create
+{
+public:
+	Create()
+	{
+		this->direction = PLUS_X;
+		this->total_distance=0;
+		this->velocity=0; 
+		this->total_angle=0;
+		this->push_bumper = NONE;
+		this->current_coord.x = 165.0;
+		this->current_coord.y = 165.0;
+		this->Straight_Run = true;
+		this->Right_Run = false;
+		this->Left_Run  = false;
+		this->Stop = true;
+		this->StopRun = false;
+		this->StopTurn = false;
+
+	}
+
+	void init()
+	{
+		std::cout << "distance & angle init" << std::endl;
+		std::cout << getDistance() << std::endl;
+		std::cout << getAngle() << std::endl;
+	}
+
+	void showInfo()
+	{
+		std::cout << "・・・現在座標・・・・・・・・・・・・・・・・（"<<this->current_coord.x << ", " << this->current_coord.y << ")" << std::endl;
+		std::cout << "・・・現在角度・・・・・・・・・・・・・・・・（"<<this->total_angle << ")" << std::endl;
+	}
+
+// public method
+
+	// 超音波センサを使ってストレートとカーブで直進
+	void goStraightCurveWithSoner();
+
+	// 超音波センサを使って壁との距離を計測
+	void checkDistToWall(float &ave_dist);
+
+	// 超音波センサを使って直進と回転の繰り返しで進む
+	void goStraightTurnWithSoner();
+
+	// 	普通に走る
+	void goStraight();
+
+	// 走らせる（超音波センサ使用）
+	void run();
+
+	// 止まる
+	void stopRun()
+	{
+		drive(0,0);
+		this->Stop = true;
+		this->init();
+	}
+
+	int driveCreate(int velocity, int distance_max);
+	float turnCreate(int velocity, int angle_max);
+	// 障害物の座標値をとりながら回転	
+	float turnCreateGettingObstacleCoord(int velocity, int angle_max, std::vector<Coordinate> &obstacle_coord );
+
+
+	// Createの現在座標（world）を計算
+	Coordinate calcCurrentCoordinate(int distance );
+
+	// 超音波センサが当たる部分の座標（world）の計算
+	Coordinate calcSonerHitPointCoordinate(float soner_distance);
+
+	// バンパーセンサが当たった部分の座標（world）の計算
+	Coordinate calcBumperHitPointCoordinate();
+
+	// 障害物に衝突したときの方向転換
+	void changeDirection();
+
+// checkState時に使用する関数
+	// バンパー衝突時
+	void doBumperHitMode(int bumper_hit, Coordinate &create, Coordinate &obstacle);
+	void doBumperHitModeAtObstacleSerch(int distance, int angle, int bumper_hit, Coordinate &create, Coordinate &obstacle);
+
+	// 通常時
+	void doNormalMode(Coordinate &create, Coordinate &obstacle, float &soner_distance);
+
+
+	// 超音波センサから距離値を得る[mm]	
+	float getDistanceBySoner();
+
+	void driveDistanceSearchingObstacle(int distance, std::vector<Coordinate> &create, std::vector<Coordinate>&obstacle, bool &Bumper_Hit);
+	void runNextPoint(Coordinate move_point, bool &Bumper_Hit, std::vector<Coordinate> &create, std::vector<Coordinate> &obstacle);
+
+	// 障害物を探索するメソッド
+	std::vector<Coordinate> searchObstacle(std::vector<Coordinate> &create);
+
+
+
+// set method
+	// createの角度をセット（テスト用）
+	void setTotalAngle(int angle)
+	{
+		this->total_angle =angle;
+	}
+
+
+	// Createの角度を更新
+	void addAngle(float angle);
+
+	// createの走行距離を更新
+	void addDistance(int distance)
+	{
+		this->total_distance +=distance;
+	}
+
+	// 現在座標の値を更新
+	void updateCurrentCoordinateXY(float X, float Y)
+	{
+		this->current_coord.x = X;
+		this->current_coord.y = Y;
+	}
+
+
+// get method
+	float getTotalAngle()	
+	{
+		return this->total_angle;
+	}
+	int getTotalDistance()	
+	{
+		return this->total_distance;
+	}
+
+
+	float getAngleFromCreate( int velocity );
+
+	int getDistanceFromCreate()
+	{
+		return getDistance();
+	}	
+
+	Coordinate getCurrentCoordinate()
+	{
+		return this->current_coord;
+	}
+
+	bool isDockFound();		//ドッキングステーションを発見したかどうか判断する 発見:true 未発見:false
+
+
+
+//public 変数
+	bool Straight_Run;
+	bool Right_Run;
+	bool Left_Run;
+	bool Stop;
+	bool StopRun;
+	bool StopTurn;
+	enum Bumper push_bumper;
+	enum IRobotDirection direction;
+
+	std::vector<float> soner_dist;
+
+private:
+	int velocity;
+	float total_angle;
+	int total_distance;
+
+	Coordinate current_coord;
+
+
+};
+
+#include "Create.cpp"
